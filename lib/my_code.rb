414c60ba64bccbe7526e0a44c724bef6e6f36f3b
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-24 03:58:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,3 +2,4 @@
 (map(dune){|n| n}) .to eq(dune)
 (map([1, 2, 3 , -9]){|n| * 2}) .to eq [2, 4, 6, -18])
 (map([1, 2, 3, -9]){|n| n * .to eq ({1, 4, 9, 81})
+(reduce(source_array, starting_point){|memo, n| memo + n}) .to eq(106)
