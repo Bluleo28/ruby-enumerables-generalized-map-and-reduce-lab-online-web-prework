acdcd5d438d42056587265c34a8709295c02fe56
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-24 04:01:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,3 +4,5 @@
 (map([1, 2, 3, -9]){|n| n * .to eq ({1, 4, 9, 81})
 (reduce(source_array, starting_point){|memo, n| memo + n}) .to eq(106)
 (reduce(source_array){|memo,n|memo && n}) .to be_truthy
+(reduce(source_array){|memo, n| memo && n}) .to be_falsy
+(reduce(source_array){|memo, n|memo||n}) .to eq(true)
